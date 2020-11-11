243958510b44454f78f6b536220a400a8e228146
Sany-alam <playerc950@gmail.com>
2020-11-11 15:41:02 +0600

    layout.bootstrap nav items moved right

--- resources/views/layout/bootstrap.blade.php
+++ resources/views/layout/bootstrap.blade.php
@@ -26,15 +26,14 @@
 <!-- Header -->
 <div id="header">
     <div class="container">
-
-        <!-- Left Side Content -->
         <div class="left-side">
-
             <!-- Logo -->
             <div id="logo">
                 <a href="index.php"><img src="{{asset('assets')}}/images/logo.jpeg" alt=""></a>
             </div>
-
+        </div>
+        <!-- Left Side Content -->
+        <div class="right-side">
             <!-- Main Navigation -->
             <nav id="navigation">
                 <ul id="responsive">
@@ -58,106 +57,6 @@
 
         </div>
         <!-- Left Side Content / End -->
-
-
-        <!-- Right Side Content / End -->
-        <div class="right-side">
-
-            <!--  User Notifications -->
-            <div class="header-widget hide-on-mobile">
-
-                <!-- Notifications -->
-                <div class="header-notifications">
-
-                    <!-- Trigger -->
-                    <div class="custom_notification">
-                        <a href="see_notification.php"><i class="icon-feather-bell"></i><span><?php echo $total_notification=1 ?></span></a>
-                    </div>
-
-                    <!-- Dropdown -->
-
-                </div>
-
-                <!-- Messages -->
-                <div class="header-notifications">
-                    <div class="header-notifications-trigger">
-                        <a href="#"><i class="icon-feather-mail"></i><span>3</span></a>
-                    </div>
-
-                    <!-- Dropdown -->
-                    <div class="header-notifications-dropdown">
-
-                        <div class="header-notifications-headline">
-                            <h4>Messages</h4>
-                            <button class="mark-as-read ripple-effect-dark" title="Mark all as read" data-tippy-placement="left">
-                                <i class="icon-feather-check-square"></i>
-                            </button>
-                        </div>
-
-                        <div class="header-notifications-content">
-                            <div class="header-notifications-scroll" data-simplebar>
-                                <ul>
-                                    <!-- Notification -->
-                                    <li class="notifications-not-read">
-                                        <a href="dashboard-messages.html">
-                                            <span class="notification-avatar status-online"><img src="{{asset('assets')}}/images/user-avatar-small-03.jpg" alt=""></span>
-                                            <div class="notification-text">
-                                                <strong>David Peterson</strong>
-                                                <p class="notification-msg-text">Thanks for reaching out. I'm quite busy right now on many...</p>
-                                                <span class="color">4 hours ago</span>
-                                            </div>
-                                        </a>
-                                    </li>
-
-                                    <!-- Notification -->
-                                    <li class="notifications-not-read">
-                                        <a href="dashboard-messages.html">
-                                            <span class="notification-avatar status-offline"><img src="{{asset('assets')}}/images/user-avatar-small-02.jpg" alt=""></span>
-                                            <div class="notification-text">
-                                                <strong>Sindy Forest</strong>
-                                                <p class="notification-msg-text">Hi Tom! Hate to break it to you, but I'm actually on vacation until...</p>
-                                                <span class="color">Yesterday</span>
-                                            </div>
-                                        </a>
-                                    </li>
-
-                                    <!-- Notification -->
-                                    <li class="notifications-not-read">
-                                        <a href="dashboard-messages.html">
-                                            <span class="notification-avatar status-online"><img src="{{asset('assets')}}/images/user-avatar-placeholder.png" alt=""></span>
-                                            <div class="notification-text">
-                                                <strong>Marcin Kowalski</strong>
-                                                <p class="notification-msg-text">I received payment. Thanks for cooperation!</p>
-                                                <span class="color">Yesterday</span>
-                                            </div>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <a href="dashboard-messages.html" class="header-notifications-button ripple-effect button-sliding-icon">View All Messages<i class="icon-material-outline-arrow-right-alt"></i></a>
-                    </div>
-                </div>
-
-            </div>
-            <!--  User Notifications / End -->
-
-            <!-- User Menu -->
-
-            <!-- User Menu / End -->
-
-            <!-- Mobile Navigation Button -->
-            <span class="mmenu-trigger">
-                <button class="hamburger hamburger--collapse" type="button">
-                    <span class="hamburger-box">
-                        <span class="hamburger-inner"></span>
-                    </span>
-                </button>
-            </span>
-
-        </div>
-        <!-- Right Side Content / End -->
 
     </div>
 </div>
